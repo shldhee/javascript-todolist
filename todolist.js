b06f0c666f11d067e3e2f1b2e1823be0e53b3af1
wacilpong <cilpong@gmail.com>
2018-02-21 19:00:39 +0900

    merge to roomy branch

--- todolist.js
+++ todolist.js
@@ -1,60 +1,66 @@
-// TODO: refactoring code to functional
-// TODO: Set singleton pattern for todolist
-var todoObj = {
-  todoList: new Array(),
-  idx: 0,
-  
-  todo() {
+// TODO: refactoring code to functinal!
+var todoList = [];
+var i = 0;
 
-  },
+function Todo() {}
 
-  render(o) {
-    var li = document.createElement('li');
+function render() {
+  console.log('------------------');
+  for (var i = 0; i < todoList.length; i++) {
+    console.log(
+      'Todo : ' + todoList[i].item + ' , 진행상태 : ' + todoList[i].state
+    );
+  }
+  console.log('------------------');
+}
 
-    // for (var i = 0; i < this.todoList.length; i++) {
-      li.append('Todo : ' + o.item + ' , 진행상태 : ' + o.state);
-      document.getElementById("checkList").append(li);
-    // }
-  },
-
-  change(item) {
-    for (var i = 0; i < todoList.length; i++) {
-      for (var key in todoList[i]) {
-        if (item === todoList[i][key]) {
-          todoList[i].state = !todoList[i].state;
-          render();
-          return;
-        }
+function changeTodo(item) {
+  for (var i = 0; i < todoList.length; i++) {
+    for (var key in todoList[i]) {
+      if (item === todoList[i][key]) {
+        todoList[i].state = !todoList[i].state;
+        render();
+        return;
       }
     }
-  },
-
-  addTodo(item) {
-    var newList = {
-      id: this.idx++,
-      item: item,
-      state: false,
-      progress: this.state ? '완료' : '진행중',
-    };
-
-    this.todoList.push(newList);
-    this.render(newList);
-  },
-
-  todoView() {
-    // Todo에게 밥먹기 추가 요청
-  },
-
-  todoItem(task) {
-    this.id = this.idx++;
-    this.task = task;
-    this.state = false;
-  },
-
-  init() {
-    this.addTodo('밥먹기');
-    this.addTodo('설거지하기');
   }
 }
 
-todoObj.init();
+function addTodo(item) {
+  var newTodoList = {
+    id: i++,
+    item: item,
+    state: false,
+    progress: this.state ? '완료' : '진행중',
+  };
+  todoList.push(newTodoList);
+  render();
+}
+
+function TodoView() {
+  // Todo에게 밥먹기 추가 요청
+}
+
+function TodoList() {}
+
+function TodoItem(title) {
+  this.id = i++;
+  this.title = title;
+  this.state = false;
+}
+
+/**
+ * TodoItem
+ * Id
+ * Title
+ * State
+ */
+
+/**
+    add  추가
+    finish 완료
+    delete 삭제?
+  */
+
+addTodo('밥먹기');
+addTodo('설거지하기');
