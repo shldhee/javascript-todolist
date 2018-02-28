33d604ec2239b999e6f3b74d174dd041fdfcc10d
LEEDUCKHEE <shldhee@yahoo.co.kr>
2018-02-28 16:35:03 +0900

    add function removeTodo, edit function render

--- todolist.js
+++ todolist.js
@@ -4,22 +4,31 @@ var todoObj = {
   todoList: [],
   idx: 0,
 
-  render(o) {
-    var li = document.createElement('li');
+  render() {
+    for(i = 0; i < this.todoList.length; i++) {
+      console.log(
+        "ID : " + this.todoList[i].id +
+        " 할일 : " + this.todoList[i].item +
+        " 상태 : " + this.todoList[i].progress
+      );
+    }
+    console.log('-----------------------------');
+    // var li = document.createElement('li');
 
     // for (var i = 0; i < this.todoList.length; i++) {
-      li.append('Todo : ' + o.item + ' , 진행상태 : ' + o.state);
-      document.getElementById("checkList").append(li);
+      // li.append('Todo : ' + o.item + ' , 진행상태 : ' + o.state);
+      // document.getElementById("checkList").append(li);
     // }
   },
 
   changeTodo(id) {
     for(var i = 0; i < this.todoList.length; i++) {
       if(this.todoList[i].id === id) {
-        this.todoList[i].state = !this.todoList[i].state;
+        this.todoList[i].state = !this.todoList[i].state; // 상태만 바꾸지 progress는 바뀌지 않는다.
         break;
       }
     }
+    this.render();
   },
 
   addTodo(item) {
@@ -31,7 +40,17 @@ var todoObj = {
     };
 
     this.todoList.push(newList);
-    this.render(newList);
+    this.render();
+  },
+
+  removeTodo(id) {
+    for(var i = 0; i < this.todoList.length; i++) {
+      if(this.todoList[i].id === id) {
+        this.todoList.splice(i,1);
+        break;
+      }
+    }
+    this.render();
   },
 
   init() {
